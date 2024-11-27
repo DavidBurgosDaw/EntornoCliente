0b9ecb2673c9daee5e0161b848b6e14349e258d8
DavidBurgosDaw <daw202505@estudiantes.salesianasnsp.es>
2024-11-27 21:11:50 +0100

    timeout y interval

---
+++ Apuntes/FuncionesExamenes/usoTimeoutYInterval.js
@@ -0,0 +1,8 @@
+setTimeout(() => {
+    console.log('Esto se ejecuta una vez después de 2 segundos');
+}, 2000);
+
+setInterval(() => {
+    console.log('Esto se ejecuta cada 2 segundos');
+}, 2000);
+
